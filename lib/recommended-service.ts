47bacf1a0047969eb9d45c6a67d69f0d962ed421
Abdelkader-gnichi <ggnichi097@gmail.com>
2023-12-23 17:38:19 +0100

    fixing hydration errors in the sidebar component

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -5,7 +5,7 @@ import { db } from "./db";
 export async function getRecommended(){
 
 
-    await new Promise(resolve => setTimeout(resolve,6000))
+    // await new Promise(resolve => setTimeout(resolve,9000))
    const users = await db.user.findMany({
         orderBy: {
             createdAt: "desc"
