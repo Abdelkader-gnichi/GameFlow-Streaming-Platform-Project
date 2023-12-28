83f1a5bec501a24a25556f0a2f7873ec8a330fa6
Abdelkader-gnichi <ggnichi097@gmail.com>
2023-12-28 18:45:47 +0100

    creating chat settings page with some extra components

---
+++ app/(Dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Switch } from "@/components/ui/switch";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { updateStream } from "@/actions/stream";
+import { Skeleton } from "@/components/ui/skeleton";
+
+type FieldTypes =  "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
+
+interface ToggleCardProps {
+    field: FieldTypes;
+    label: string;
+    value: boolean;
+}
+
+export function ToggleCard({field, label, value= false}: ToggleCardProps){
+    const [isPending, startTransition] = useTransition();
+    function onChange(){
+        startTransition(() =>{
+            updateStream({[field]: !value})
+            .then(() => toast.success("Chat setting updated!"))
+            .catch(() => toast.error("Something went wrong"))
+        })
+    }
+    return <div className="rounded-xl bg-muted p-6">
+                <div className="flex items-center justify-between">
+                    <p className="font-semibold shrink-0">
+                        {label}
+                    </p>
+                    <div className="space-y-2">
+                        <Switch disabled={isPending} onCheckedChange={onChange} checked={value}>
+                            {value ? "On" : "Off"}
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+}
+
+export function ToggleCardSkeleton(){
+    return (<Skeleton className="rounded-xl p-10 w-full" />);
+}
