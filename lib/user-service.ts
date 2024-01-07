a8af3e812c80769c1c5ea7181f79f1e283fcbe08
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-01-07 16:09:15 +0100

    implement and finishing up the home page component

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,28 +1,45 @@
-import { db } from "./db";
+import { db } from "@/lib/db"
 
-export async function getUserByUsername(username: string){
-
-    const user = await db.user.findUnique({
-        where: { username },
-        include: { 
-            stream: true,
-            _count: {
-                select: {
-                    followedBy: true,
-                },
-            },
+export const getUserByUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+      externalUserId: true,
+      username: true,
+      bio: true,
+      imgUrl: true,
+      stream: {
+        select: {
+          id: true,
+          isLive: true,
+          isChatDelayed: true,
+          isChatEnabled: true,
+          isChatFollowersOnly: true,
+          thumbnailUrl: true,
+          name: true,
         },
-    });
+      },
+      _count: {
+        select: {
+          followedBy: true,
+        },
+      },
+    },
+  });
 
-    return user
-}
+  return user;
+};
 
-export async function getUserById(id: string){
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true,
+    },
+  });
 
-    const user = await db.user.findUnique({
-        where: { id },
-        include: { stream: true}
-    })
-
-    return user
-}
+  return user;
+};
